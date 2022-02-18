b04ab5c113ac9761138aeae7a0118990269995a3
KyuhyunJo92 <52312016+KyuhyunJo92@users.noreply.github.com>
2022-02-18 10:30:37 +0100

    Add files via upload

---
+++ Temp.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace MaxTemp
+{
+    public class Temp 
+    {
+        public string sensorName 
+        {
+            get
+            {
+                return this.sensorName;
+            }
+            set
+            {
+                if (this.sensorName == value)
+                {
+                    return;
+                }
+                this.sensorName = value;
+            }
+        }
+        public string time
+        {
+            get
+            {
+                return this.time;
+            }
+            set
+            {
+                if (this.time == value)
+                {
+                    return;
+                }
+                this.time = value;
+            }
+        }
+        public string temp
+        {
+            get
+            {
+                return this.temp;
+            }
+            set
+            {
+                if (this.temp == value)
+                {
+                    return;
+                }
+                this.temp = value;
+            }
+        }
+        public Temp(string _sensorName, string _time, string _temp) 
+        {
+            sensorName = _sensorName;
+            time = _time;
+            temp = _temp;
+        }
+
+        public Temp() { }
+        
+        
+    }
+}
