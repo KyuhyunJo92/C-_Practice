59e204950badbcfe7320200c6a5c8a7750af8eb4
KyuhyunJo92 <52312016+KyuhyunJo92@users.noreply.github.com>
2022-05-13 12:47:24 +0200

    Add files via upload

---
+++ Person/V4.1(strcpy_s,strcat_s!!)/Common.h
@@ -0,0 +1,16 @@
+#pragma once
+
+#include "stdafx.h"
+#include <iostream>
+#include <map>
+#include <string>
+#include <fstream>
+#include <stack>
+#include <filesystem>
+#include <sstream>
+#include "sqlite3.h"
+#include <cstring>//strtok() method.
+#include <stdio.h>
+
+
+using namespace std;
